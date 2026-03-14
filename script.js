20594bd87ccf6633de8ffabb2f2cef77b4a4a01d
dchaubey560 <dchaubey560@gmail.com>
2026-03-14 10:22:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+let prompt = document.querySelector("#prompt");
+let submitbtn= document.querySelector("#submit");
+let chatContainer=document.querySelector(".chat-container")
+let imagebtn=document.querySelector("#image")
+let image=document.querySelector("#image img")
+let imageinput=document.querySelector("#image input")
+
+ const Api_url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyA_UufaIvUB77UW8-3SeLwJ1WVHYvlaWT0"
+let user={
+    message:null,
+    file:{
+      mime_type:null,
+      data:null
+    }
+}
+
+async function generateResponse(aiChatBox) {
+    let text=aiChatBox.querySelector(".ai-chat-area")
+    
+ let resquestOption={
+    method:"POST",
+    headers:{'Content-Type': 'application/json'},
+    body:JSON.stringify({"contents": [
+         {"parts": [{ "text": user.message},(user.file.data?[{"inline_data":user.file}]:[])
+         ]
+    }] 
+    })
+    }
+      try{
+        let response=await fetch(Api_url,resquestOption)
+        let data=await response.json()
+       let apiResponse = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+        text.innerHTML=apiResponse
+       {
+}
+      }
+      catch(error){
+        console.log(error);
+      }
+      finally{
+         chatContainer.scroll({
+    top: chatContainer.scrollHeight,
+    behavior: "smooth"
+         });
+     image.src=`img.svg`
+    image.classList.remove("choose")
+    user.file={}
+      }
+     
+}
+
+function createChatBox(html,classes){
+    let div=document.createElement("div")
+    div.innerHTML=html
+    div.classList.add(classes)
+    return div
+}
+function handlechatResponse(userMessage){
+    user.message=userMessage
+    let html= `<img src="user_image.jpg" alt="User Avatar" id="userImage" width="30"></img>
+    <div class="user-chat-area">
+    ${user.message}
+    ${user.file.data?`<img src="data:${user.file.mime_type};base64,${user.file.data}" class="chooseimg"/>`:""}
+    </div>`
+ prompt.value=""
+let userChatBox=createChatBox(html,"user-chat-box")
+chatContainer.appendChild(userChatBox)
+
+chatContainer.scroll({
+  top: chatContainer.scrollHeight,
+  behavior: "smooth"
+});
+
+setTimeout(()=>{
+    let html= `<img src="ai_image.jpg" alt="AI Avatar" id="aiImage" width="50">
+        <div class="ai-chat-area">
+          <img src="loading.webp" alt="" class="load" width="50">
+      </div>`
+      let aiChatBox=createChatBox(html,"ai-chat-box")
+      chatContainer.appendChild(aiChatBox)
+      generateResponse(aiChatBox)
+},600);
+}
+prompt.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    handlechatResponse(prompt.value);
+  }
+})
+
+submitbtn.addEventListener("click",()=>{
+  handlechatResponse(prompt.value)
+})
+
+imageinput.addEventListener("change",()=>{
+  const file=imageinput.files[0]
+  if(!file) return
+  let reader=new FileReader()
+  reader.onload=(e)=>{
+   let base64String =e.target.result.split(",")[1]
+   user.file={
+    mime_type:file.type,
+    data:base64String
+   }
+   image.src=`data:${user.file.mime_type};base64,${user.file.data}`
+   image.classList.add("choose")
+  }
+  reader.readAsDataURL(file)
+}
+)
+ 
+
+imagebtn.addEventListener("click",()=>{
+  imagebtn.querySelector("input").click()
+})
